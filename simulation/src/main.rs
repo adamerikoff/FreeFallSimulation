a5a51f3a5058e9b496a0a748aca564374f90fa68
Adam Erikuly <adamerikoff@protonmail.com>
2025-03-13 19:50:24 +0500

    window get's spawned

--- simulation/src/main.rs
+++ simulation/src/main.rs
@@ -1,12 +1,50 @@
-// Declare the vector3 module
-mod vector3;
+use winit::application::ApplicationHandler;
+use winit::event::WindowEvent;
+use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
+use winit::window::{Window, WindowId};
+use winit::dpi::LogicalSize;
 
-// Bring Vector3 into scope
-use vector3::Vector3;
+
+#[derive(Default)]
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window_attributes = Window::default_attributes()
+            .with_title("FreeFallSimulation")
+            .with_inner_size(LogicalSize::new(1200, 800));
+
+        if let Ok(window) = event_loop.create_window(window_attributes) {
+            self.window = Some(window);
+        } else {
+            eprintln!("Failed to create window");
+            event_loop.exit(); // Exit if window creation fails
+        }
+    }
+
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+        match event {
+            WindowEvent::CloseRequested => {
+                println!("The close button was pressed; stopping");
+                event_loop.exit();
+            },
+            WindowEvent::RedrawRequested => {
+                self.window.as_ref().unwrap().request_redraw();
+            }
+            _ => (),
+        }
+    }
+}
 
 fn main() {
-    let v1: Vector3 = Vector3::new(1.0, 2.0, 3.0);
-    let v2: Vector3 = Vector3::new(4.0, 5.0, 6.0);
-    let sum: Vector3 =  v1.add(&v2);
-    println!("Sum: {:?}", sum);
+    if let Ok(event_loop) = EventLoop::new() {
+        event_loop.set_control_flow(ControlFlow::Poll);
+
+        let mut app = App::default();
+        event_loop.run_app(&mut app);
+    } else {
+        eprintln!("Failed to create event loop");
+    }
 }
